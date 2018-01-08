884442c328aa554bdd359a99a30830c50d440ae9
Kahsius <cassius81.geek@free.fr>
2018-01-08 11:14:29 +0000

    First prototype reconstruction system

--- collabAE.py
+++ collabAE.py
@@ -1,37 +1,165 @@
-from torch.autograd import Variable
 import torch.optim as optim
 import pandas as pd
 import numpy as np
+import sys
+import functools as ft
 from sklearn.preprocessing import scale
 from libCollabAE import *
 
-PTEST = .1
-NSTEPS = 100
+# PARAMETERS
+VERBOSE = False
 
+# HYPERPARAMETERS
+PTEST = .1
+NSTEPS = 5000
+NVIEWS = 3
+LAYERS_AE = [30, 10]
+LAYERS_LINKS = [30, 30]
+LEARNING_RATE_AE = 0.03
+LEARNING_RATE_LINKS = 0.06
+
+# DATA PREPROCESSING
 data = pd.read_csv("data/wdbc.data", header=None).values[:,2:]
 data = np.array(data, dtype='float')
 data = scale(data)
 np.random.shuffle(data)
 
+# DATA INFORMATIONS
 dimData = data.shape[1]
 nData = data.shape[0]
 nTest = int(nData * PTEST)
-print("nData : " + str(nData))
+print("\nnData : " + str(nData))
 print("nTest : " + str(nTest))
 
+# TRAIN AND TEST SETS
 test_data = Variable(torch.from_numpy(data[:nTest,:]).float())
 train_data = Variable(torch.from_numpy(data[nTest:,:]).float())
 
-aenet = AENet([dimData, 30, 10])
-criterion = nn.MSELoss()
-optimizer = optim.SGD(aenet.parameters(), lr=0.03)
+indexes = getIndexesViews(dimData, NVIEWS)
+print("Indexes : " + str(indexes))
+train_datasets = getViewsFromIndexes(train_data, indexes)
+test_datasets = getViewsFromIndexes(test_data, indexes)
 
-for epoch in range(NSTEPS):
-	optimizer.zero_grad()
-	outputs = aenet(test_data)
-	loss = criterion(outputs, test_data)
-	if epoch % 100 == 0 : print("Loss " + str(epoch) + " : " + str(loss.data[0]))
-	outputs = aenet(train_data)
-	loss = criterion(outputs, train_data)
-	loss.backward()
-	optimizer.step()
+# LEARNING ALL THE MODELS AND GET THE CODES
+models = list()
+codes = list()
+codes_test = list()
+
+for i in range(NVIEWS):
+	print("Learning view " + str(i))
+	dataset = train_datasets[i]
+	dataset_test = test_datasets[i]
+	dimData = indexes[i+1] - indexes[i]
+
+	# MODEL DEFINITION
+	net = AENet( [dimData] + LAYERS_AE )
+	criterion = nn.MSELoss()
+	optimizer = optim.SGD(net.parameters(), lr=LEARNING_RATE_AE)
+
+	# LEARNING
+	for epoch in range(NSTEPS):
+
+		if VERBOSE:
+			# Test information
+			outputs = net(dataset_test)
+			loss = criterion(outputs, dataset_test)
+			if epoch % 100 == 0 : 
+				print("Test Loss " + str(epoch) + " : " + str(loss.data[0]))
+		
+		optimizer.zero_grad()
+		
+		# Train information
+		outputs = net(dataset)
+		loss = criterion(outputs, dataset)
+
+		# Parameters optimization
+		loss.backward()
+		optimizer.step()
+
+	outputs = net(dataset_test)
+	loss = criterion(outputs, dataset_test)
+	print("View " + str (i) + " - test loss : " + str(loss.data[0]))
+
+	models.append(net)
+	print("Encoding training and test dataset...")
+	# Codes gathering
+	code = net.encode(dataset)
+	code = Variable(code.data, requires_grad = False)
+	codes.append(code)
+
+	code_test = net.encode(dataset_test)
+	code_test = Variable(code_test.data, requires_grad = False)
+	codes_test.append(code_test)
+
+# LEARNING OF THE LINKS
+links = list()
+for i in range(NVIEWS):
+	links.append(list())
+
+for i in range(NVIEWS):
+	for j in range(NVIEWS):
+		if i == j :
+			links[i].append([])
+		else :
+
+			# GET THE CODE TO LINK
+			data_in = codes[i]
+			data_out = codes[j]
+
+			data_test_in = codes_test[i]
+			data_test_out = codes_test[j]
+
+			dimData_in = data_in.size()[1]
+			dimData_out = data_out.size()[1]
+
+			# DEFINE THE MODEL
+			print("Link " + str(i) + " ~ " + str(j))
+			net = LinkNet( [dimData_in] + LAYERS_LINKS + [dimData_out] )
+			criterion = nn.MSELoss()
+			optimizer = optim.SGD( net.parameters(), lr=LEARNING_RATE_LINKS)
+
+			# LEARNING
+			for epoch in range(NSTEPS):
+				# Test information
+				if VERBOSE:
+					outputs = net(data_test_in)
+					loss = criterion(outputs, data_test_out)
+					if epoch % 100 == 0 : 
+						print("Test Loss " + str(epoch) + " : " + str(loss.data[0]))
+
+				optimizer.zero_grad()
+				
+				# Train information
+				outputs = net(data_in)
+				loss = criterion(outputs, data_out)
+
+				# Parameters optimization
+				loss.backward()
+				optimizer.step()
+
+			# print(net(data_test_in).data[1:10,:])
+			# print(data_test_in.data[1:10,:])
+
+			outputs = net(data_test_in)
+			loss = criterion(outputs, data_test_out)
+			print("Link " + str(i) + " ~ " + str(j) + " - test loss : " + str(loss.data[0]))
+
+			links[i].append(net)
+
+# TESTING THE RECONSTRUCTION
+for i in range(NVIEWS):
+	codes = list()
+	for j in range(NVIEWS):
+		if i != j :
+			code_externe = models[j].encode(test_datasets[j])
+			code_interne = links[j][i](code_externe)
+			codes.append(code_interne)
+
+	code_moyen = ft.reduce(lambda x, y: (x.data+y.data)/(NVIEWS-1), codes)
+	code_moyen = Variable(code_moyen)
+
+	indiv_reconstruit = models[i].decode(code_moyen)
+	criterion = nn.MSELoss()
+	loss = criterion(indiv_reconstruit, test_datasets[i])
+
+	print("Reconstruction error view " + str(i) + " : " + str(loss.data[0]))
--- libCollabAE.py
+++ libCollabAE.py
@@ -1,22 +1,26 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.autograd import Variable
+
+# ===================================================================== 
 
 class AENet(nn.Module):
 	def __init__(self, arrDim):
 		super(AENet, self).__init__()
 		self.n_hidden_layers = len(arrDim) - 1
-		self.layers = list()
 
 		# Encoding functions
 		for i in range(self.n_hidden_layers) :
 			f = nn.Linear(arrDim[i], arrDim[i+1])
-			self.layers.append(f)
+			name = "fct" + str(i)
+			setattr(self, name, f)
 
 		# Decoding functions
 		for i in range(self.n_hidden_layers) :
 			f = nn.Linear(arrDim[-i-1], arrDim[-i-2])
-			self.layers.append(f)
+			name = "fct" + str(i + self.n_hidden_layers)
+			setattr(self, name, f)
 
 	def forward(self, x):
 		x = self.encode(x)
@@ -24,13 +28,60 @@ class AENet(nn.Module):
 		return x
 
 	def encode(self, x):
-		for i in range(self.n_hidden_layers) :
-			x = F.relu(self.layers[i](x))
+		for i in range(self.n_hidden_layers - 1) :
+			name = "fct" + str(i)
+			fct = getattr(self, name)
+			x = F.relu(fct(x))
+		name = "fct" + str(self.n_hidden_layers - 1)
+		fct = getattr(self, name)
+		x = F.relu(fct(x))
 		return x
 
 	def decode(self, x):
 		for i in range(self.n_hidden_layers - 1) :
-			x = F.relu(self.layers[n_hidden_layers + i](x))
-		x = self.layers[-1](x)
+			name = "fct" + str(i + self.n_hidden_layers)
+			fct = getattr(self, name)
+			x = F.relu(fct(x))
+		name = "fct" + str(2*self.n_hidden_layers - 1)
+		fct = getattr(self, name)
+		x = fct(x)
 		return x
+
+# ===================================================================== 
+
+class LinkNet(nn.Module):
+	def __init__(self, arrDim):
+		super(LinkNet, self).__init__()
+		self.n_hidden_layers = len(arrDim) - 2
+
+		for i in range( len(arrDim) - 1 ):
+			f = nn.Linear( arrDim[i], arrDim[i+1] )
+			name = "fct" + str(i)
+			setattr(self, name, f)
+
+	def forward(self, x):
+		for i in range(self.n_hidden_layers):
+			name = "fct" + str(i)
+			fct = getattr(self, name)
+			x = F.relu(fct(x))
+		name = "fct" + str(self.n_hidden_layers)
+		fct = getattr(self, name)
+		x = fct(x)
+		return x
+
+# =====================================================================
+
+def getIndexesViews(dimData, nViews):
+	indexes = list()
+	for i in range(nViews+1):
+		indexes.append(round(i*dimData/nViews))
+	return indexes
+
+# =====================================================================
+
+def getViewsFromIndexes(data, indexes):
+	views = list()
+	for i in range(len(indexes)-1):
+		views.append(data[:,indexes[i]:indexes[i+1]])
+	return views
 
